733fe6203f2b281686271644ba5870db5508a051
DannyMyles <dancun.wawire@student.moringaschool.com>
2021-11-13 14:52:32 +0300

    added secret keys

--- flaskblog.py
+++ flaskblog.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '05218339f677c59429eff4698cd343ed'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
